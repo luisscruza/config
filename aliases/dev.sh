36e6045f554f5433c0346843d0566d70e2cc8e88
Luis De la Cruz <98129640+cruzmediaorg@users.noreply.github.com>
2024-10-02 18:42:34 +0200

    new shell aliases

--- aliases/dev.sh
+++ aliases/dev.sh
@@ -7,7 +7,19 @@ alias compu='composer update'
 alias pa='php artisan'
 alias pas='php artisan serve'
 alias pam='php artisan make:model'
+alias tinker='php artisan tinker'
 
 # NPM
 alias nrd='npm run dev'
 alias nrb='npm run build'
+
+
+# Podman
+alias p='podman'
+# alias docker='podman' 
+
+# Mac OS
+alias finder='open -a Finder'
+
+# Custom
+alias localip='ipconfig getifaddr en0'
--- aliases/git.sh
+++ aliases/git.sh
@@ -1,11 +1,15 @@
-alias wip='git add . && git commit -m "WIP"'
-alias wipd='git add . && git commit -m "WIP" && git push'
-alias ga='git add .'
-alias gc='git checkout'
-alias commit='git commit -m' # commit "message"
 
-alias uncommit='git reset --soft HEAD^'
+# Git global alias
+alias g='git'
 
-alias gpl='git pull'
-alias gp='git push'
-alias gps='git push --set-upstream origin $(git_current_branch)'
+alias wip='g add . && g commit -m "WIP"'
+alias wipd='g add . && g commit -m "WIP" && g push'
+alias ga='g add .'
+alias gc='g checkout'
+alias commit='g commit -m' # commit "message"
+
+alias uncommit='g reset --soft HEAD^'
+
+alias gpl='g pull'
+alias gp='g push'
+alias gps='g push --set-upstream origin $(git_current_branch)'
